f01eeaeca81ad19d1d993702332e7cfea0d2d6e1
Pedro Araujo <106033591+psilvagg@users.noreply.github.com>
2024-09-15 20:38:28 -0300

    .
    .

--- telaInicial.py
+++ telaInicial.py
@@ -1,47 +1,406 @@
-import time
-import telebot as tl
-import os
 import smtplib
-import email.message
+import time
+import os
+import telebot as tl
+from email.message import EmailMessage
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
+def enviarEmail(email):
+    bodyEmail = """
+    <!doctype html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Confirmação de inscrição!</title>
+    <style>
+      /* -------------------------------------
+          GLOBAL RESETS
+      ------------------------------------- */
+      img {
+        border: none;
+        -ms-interpolation-mode: bicubic;
+        max-width: 100%; }
+
+      body {
+        background-color: #f6f6f6;
+        font-family: sans-serif;
+        -webkit-font-smoothing: antialiased;
+        font-size: 14px;
+        line-height: 1.4;
+        margin: 0;
+        padding: 0;
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%; }
+
+      table {
+        border-collapse: separate;
+        mso-table-lspace: 0pt;
+        mso-table-rspace: 0pt;
+        width: 100%; }
+        table td {
+          font-family: sans-serif;
+          font-size: 14px;
+          vertical-align: top; }
+
+      /* -------------------------------------
+          BODY & CONTAINER
+      ------------------------------------- */
+
+      .body {
+        background-color: #f6f6f6;
+        width: 100%; }
+
+      /* Set a max-width, and make it display as block so it will automatically stretch to that width, but will also shrink down on a phone or something */
+      .container {
+        display: block;
+        Margin: 0 auto !important;
+        /* makes it centered */
+        max-width: 580px;
+        padding: 10px;
+        width: 580px; }
+
+      /* This should also be a block element, so that it will fill 100% of the .container */
+      .content {
+        box-sizing: border-box;
+        display: block;
+        Margin: 0 auto;
+        max-width: 580px;
+        padding: 10px; }
+
+      /* -------------------------------------
+          HEADER, FOOTER, MAIN
+      ------------------------------------- */
+      .main {
+        background: #ffffff;
+        border-radius: 3px;
+        width: 100%; }
+
+      .wrapper {
+        box-sizing: border-box;
+        padding: 20px; }
+
+      .content-block {
+        padding-bottom: 10px;
+        padding-top: 10px;
+      }
+
+      .footer {
+        clear: both;
+        Margin-top: 10px;
+        text-align: center;
+        width: 100%; }
+        .footer td,
+        .footer p,
+        .footer span,
+        .footer a {
+          color: #999999;
+          font-size: 12px;
+          text-align: center; }
+
+      /* -------------------------------------
+          TYPOGRAPHY
+      ------------------------------------- */
+      h1,
+      h2,
+      h3,
+      h4 {
+        color: #000000;
+        font-family: sans-serif;
+        font-weight: 400;
+        line-height: 1.4;
+        margin: 0;
+        Margin-bottom: 30px; }
+
+      h1 {
+        font-size: 35px;
+        font-weight: 300;
+        text-align: center;
+        text-transform: capitalize; }
+
+      p,
+      ul,
+      ol {
+        font-family: sans-serif;
+        font-size: 14px;
+        font-weight: normal;
+        margin: 0;
+        Margin-bottom: 15px; }
+        p li,
+        ul li,
+        ol li {
+          list-style-position: inside;
+          margin-left: 5px; }
+
+      a {
+        color: #3498db;
+        text-decoration: underline; }
+
+      /* -------------------------------------
+          BUTTONS
+      ------------------------------------- */
+      .btn {
+        box-sizing: border-box;
+        width: 100%; }
+        .btn > tbody > tr > td {
+          padding-bottom: 15px; }
+        .btn table {
+          width: auto; }
+        .btn table td {
+          background-color: #ffffff;
+          border-radius: 5px;
+          text-align: center; }
+        .btn a {
+          background-color: #ffffff;
+          border: solid 1px #3498db;
+          border-radius: 5px;
+          box-sizing: border-box;
+          color: #3498db;
+          cursor: pointer;
+          display: inline-block;
+          font-size: 14px;
+          font-weight: bold;
+          margin: 0;
+          padding: 12px 25px;
+          text-decoration: none;
+          text-transform: capitalize; }
+
+      .btn-primary table td {
+        background-color: #3498db; }
+
+      .btn-primary a {
+        background-color: #3498db;
+        border-color: #3498db;
+        color: #ffffff; }
+
+      /* -------------------------------------
+          OTHER STYLES THAT MIGHT BE USEFUL
+      ------------------------------------- */
+      .last {
+        margin-bottom: 0; }
+
+      .first {
+        margin-top: 0; }
+
+      .align-center {
+        text-align: center; }
+
+      .align-right {
+        text-align: right; }
+
+      .align-left {
+        text-align: left; }
+
+      .clear {
+        clear: both; }
+
+      .mt0 {
+        margin-top: 0; }
+
+      .mb0 {
+        margin-bottom: 0; }
+
+      .preheader {
+        color: transparent;
+        display: none;
+        height: 0;
+        max-height: 0;
+        max-width: 0;
+        opacity: 0;
+        overflow: hidden;
+        mso-hide: all;
+        visibility: hidden;
+        width: 0; }
+
+      .powered-by a {
+        text-decoration: none; }
+
+      hr {
+        border: 0;
+        border-bottom: 1px solid #f6f6f6;
+        Margin: 20px 0; }
+
+      /* -------------------------------------
+          RESPONSIVE AND MOBILE FRIENDLY STYLES
+      ------------------------------------- */
+      @media only screen and (max-width: 620px) {
+        table[class=body] h1 {
+          font-size: 28px !important;
+          margin-bottom: 10px !important; }
+        table[class=body] p,
+        table[class=body] ul,
+        table[class=body] ol,
+        table[class=body] td,
+        table[class=body] span,
+        table[class=body] a {
+          font-size: 16px !important; }
+        table[class=body] .wrapper,
+        table[class=body] .article {
+          padding: 10px !important; }
+        table[class=body] .content {
+          padding: 0 !important; }
+        table[class=body] .container {
+          padding: 0 !important;
+          width: 100% !important; }
+        table[class=body] .main {
+          border-left-width: 0 !important;
+          border-radius: 0 !important;
+          border-right-width: 0 !important; }
+        table[class=body] .btn table {
+          width: 100% !important; }
+        table[class=body] .btn a {
+          width: 100% !important; }
+        table[class=body] .img-responsive {
+          height: auto !important;
+          max-width: 100% !important;
+          width: auto !important; }}
+
+      /* -------------------------------------
+          PRESERVE THESE STYLES IN THE HEAD
+      ------------------------------------- */
+      @media all {
+        .ExternalClass {
+          width: 100%; }
+        .ExternalClass,
+        .ExternalClass p,
+        .ExternalClass span,
+        .ExternalClass font,
+        .ExternalClass td,
+        .ExternalClass div {
+          line-height: 100%; }
+        .apple-link a {
+          color: inherit !important;
+          font-family: inherit !important;
+          font-size: inherit !important;
+          font-weight: inherit !important;
+          line-height: inherit !important;
+          text-decoration: none !important; }
+        .btn-primary table td:hover {
+          background-color: #34495e !important; }
+        .btn-primary a:hover {
+          background-color: #34495e !important;
+          border-color: #34495e !important; } }
+
+    </style>
+  </head>
+  <body class="">
+    <table border="0" cellpadding="0" cellspacing="0" class="body">
+      <tr>
+        <td>&nbsp;</td>
+        <td class="container">
+          <div class="content">
+
+            <!-- START CENTERED WHITE CONTAINER -->
+            <span class="preheader">Confirmação de inscrição, HelperBot.</span>
+            <table class="main">
+
+              <!-- START MAIN CONTENT AREA -->
+              <tr>
+                <td class="wrapper">
+                  <table border="0" cellpadding="0" cellspacing="0">
+                    <tr>
+                      <td>
+                        <p>Olá!</p>
+                        <p>Por meio deste E-Mail venho informa-ló que sua inscrição foi realizada com sucesso!</p>
+                        <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+                          <tbody>
+                            <tr>
+                              <td align="left">
+                                <table border="0" cellpadding="0" cellspacing="0">
+                                  <tbody>
+                                    <tr>
+                                      <td> <a href="https://link.mercadopago.com.br/psilvagg" target="_blank">Contribuir</a> </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <p>É gratificante saber  o seu interesse! XD</p>
+                        <p><b>BotHelper, psgg</b></p>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+
+            <!-- END MAIN CONTENT AREA -->
+            </table>
+
+            <!-- START FOOTER -->
+            <div class="footer">
+              <table border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td class="content-block">
+                    <span class="apple-link">Salvador, Bahia, Brasil </span>
+                    <br> Suporte? <a href="https://linktr.ee/pedrosxz">Aqui</a>.
+                  </td>
+                </tr>
+                <tr>
+                </tr>
+              </table>
+            </div>
+            <!-- END FOOTER -->
+
+          <!-- END CENTERED WHITE CONTAINER -->
+          </div>
+        </td>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
+  </body>
+</html>
+"""
+
+    #EmailMessage
+    msg = EmailMessage()
+    msg['Subject'] = 'Obrigado por sua inscrição!'
+    msg['From'] = f'HelperBot <helperbot@outlook.com.br>'
+    msg['To'] = email
+    msg.set_content(bodyEmail, subtype='html')
+    password = 'blcntqshzvawjtnr'
+
+    # Configuração do servidor SMTP
+    with smtplib.SMTP('smtp.office365.com', 587) as s:
+        s.starttls()
+        s.login('helperbot@outlook.com.br', password)  # Use apenas o e-mail
+        s.send_message(msg)
+
+    print('E-mail enviado com sucesso!')
 
 
-while 1:
+while True:
     print("Carregando...")
-    time.sleep(5)
-    os.system('cls')
-    print("Seja Bem-Vindo!\nSuporte:discord.gg/VDsf74sEgz\n")
+    time.sleep(2)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Seja Bem-Vindo!\nSuporte: discord.gg/VDsf74sEgz\n")
     email = input("> Cadastre-se digitando seu Email: ")
-    enviar_email(email)
+    enviarEmail(email)
     print("> Gere o TOKEN do seu BOT através do BotFather --> (https://t.me/BotFather)")
-    tokenUser = (input("Insira o TOKEN do seu BOT gerado pelo BotFather: "))
+    tokenUser = input("Insira o TOKEN do seu BOT gerado pelo BotFather: ")
     if tokenUser == '' or len(tokenUser) < 10:
-        os.system('cls')
+        os.system('cls' if os.name == 'nt' else 'clear')
         print("Verificando TOKEN...")
-        time.sleep(5)
+        time.sleep(2)
         print("Quase lá!")
-        time.sleep(3)
+        time.sleep(1)
         print("> O token inserido é inválido. Tente novamente!")
         print(">> Aperte CTRL + C simultaneamente para finalizar.")
     else:
-        bot = tl.TeleBot(tokenUser)
-        os.system('cls')
-        print("Verificando TOKEN...")
-        time.sleep(5)
-        print("Quase lá!")
-        time.sleep(3)
-        print("Token Validado com sucesso! Seu bot está online!")
-        break
-
-
-
-
-
-
-
-
+        try:
+            bot = tl.TeleBot(tokenUser)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Verificando TOKEN...")
+            time.sleep(2)
+            print("Quase lá!")
+            time.sleep(1)
+            print("Token validado com sucesso! Seu bot está online!")
+            break
+        except Exception as e:
+            print(f"Erro ao validar o token: {e}")
+            print("Tente novamente!")
 
 #Dicionário para armazenar os IDs das mensagens
 messages = {}
